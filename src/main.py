d273c6918d7a88f6c682a5dbc069b076be27f2cd
mjrao <mjrao520@gmail.com>
2017-04-27 17:31:06 +0800

    append

--- src/main.py
+++ src/main.py
@@ -5,13 +5,34 @@ __time__ = '2017/4/27'
 
 
 import web
-import utils
+#import utils
+import json
 
 urls = (
-    'phonebooks','phonebooks',
+    '/phonebooks','Phonebooks',
 )
 
+class Phonebooks:
+    def POST(self):
+        data = web.data();
+        req_json= json.loads(data)
+        opid = req_json.get('opid',0)
+        if opid == 1001:
+            response={'ret':0,'msg':'success'}
+            return json.dumps(response)
+            pass
+        elif opid == 1002:
+            response={'ret':0,'msg':'success'}
+            return json.dumps(response)
+            pass
+        else:
+            return json.dumps({'ret':-1,'msg':'no method'})
+            pass
 
+
+app = web.application(urls, globals())
+application = app.wsgifunc()
 
 if __name__=='__main__':
+    app.run()
     pass
